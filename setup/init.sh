7a68da2527fb4d0323fd5da4efc315cf196a9dcf
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-04-05 11:06:05 -0900

    Update init.sh

--- setup/init.sh
+++ setup/init.sh
@@ -8,7 +8,8 @@ echo 'keep in mind the default answer is yes...'
 read -p 'Need to add Repo? y/n> ' repo
 read -p 'Install tools? y/n>' tools
 read -p 'Create services & template? y/n>' serv
-
+#ssl
+    sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
 if [ $repo == "n"]
 then
     echo "skipping repo..."
@@ -47,14 +48,39 @@ then
 else  
     #nginx
     systemctl enable nginx
+    touch /etc/nginx/snippets/self-signed.conf
+    echo"ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;" >> /etc/nginx/snippets/self-signed.conf 
+    echo"ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;" >> /etc/nginx/snippets/self-signed.conf 
+    touch /etc/nginx/snippets/ssl-params.conf
+    echo "ssl_protocols TLSv1.2;" > /etc/nginx/snippets/ssl-params.conf
+    echo "ssl_prefer_server_ciphers on;" >> /etc/nginx/snippets/ssl-params.conf
+    echo "ssl_dhparam /etc/ssl/certs/dhparam.pem;" >> /etc/nginx/snippets/ssl-params.conf
+    echo "ssl_ciphers ECDHE-RSA-AES256-GCM-SHA512:DHE-RSA-AES256-GCM-SHA512:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384;" >> /etc/nginx/snippets/ssl-params.conf
+    echo "ssl_ecdh_curve secp384r1; # Requires nginx >= 1.1.0" >> /etc/nginx/snippets/ssl-params.conf
+    echo "ssl_session_timeout  10m;" >> /etc/nginx/snippets/ssl-params.conf
+    echo "ssl_session_cache shared:SSL:10m;" >> /etc/nginx/snippets/ssl-params.conf
+    echo "ssl_session_tickets off; # Requires nginx >= 1.5.9" >> /etc/nginx/snippets/ssl-params.conf
+    echo "# ssl_stapling on; # Requires nginx >= 1.3.7" >> /etc/nginx/snippets/ssl-params.conf
+    echo "# ssl_stapling_verify on; # Requires nginx => 1.3.7" >> /etc/nginx/snippets/ssl-params.conf
+    echo "resolver 8.8.8.8 8.8.4.4 valid=300s;" >> /etc/nginx/snippets/ssl-params.conf
+    echo "resolver_timeout 5s;" >> /etc/nginx/snippets/ssl-params.conf
+    echo "add_header X-Frame-Options DENY;" >> /etc/nginx/snippets/ssl-params.conf
+    echo "add_header X-Content-Type-Options nosniff;" >> /etc/nginx/snippets/ssl-params.conf
+    echo "add_header X-XSS-Protection "1; mode=block";" >> /etc/nginx/snippets/ssl-params.conf
+  
     #basic vpn
     sudo wget --no-check-certificate https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-US2.zip
     unzip VPNBook.com-OpenVPN-US2.zip
     sudo rm -rf VPNBook.com-OpenVPN-US2.zip vpnbook-us2-tcp80.ovpn vpnbook-us2-tcp443.ovpn vpnbook-us2-udp53.ovpn 
     
+    
     #tmeplate
     touch /opt/mkservice.txt
-    echo "[Unit]\nDescriptoin= Your description here...\n\n[Service]\nExecStart=/path/to/script.script\n\n[Install]\WantedBy=multi-user.target\n\n#Don't forget to run 'systemctl daemon-reload', or just reboot" > /opt/mkservice.txt
+    echo "[Unit]
+    Descriptoin= Your description here...
+    [Service]\nExecStart=/path/to/script.script
+    [Install]\WantedBy=multi-user.target
+    #Don't forget to run 'systemctl daemon-reload', or just reboot" > /opt/mkservice.txt
 fi
 
 echo "Done!"
