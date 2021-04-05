6cb604fc0ac6d77f91e008b7258f53af7d2f115b
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-04-05 11:12:05 -0900

    Update init.sh

--- setup/init.sh
+++ setup/init.sh
@@ -16,7 +16,6 @@ then
 else
     echo 'adding apt repo'
     #repo
-    echo "" > /etc/apt/sources.list
     apt-key adv --keyserver pool.sks-keyservers.net --recv-keys ED444FF07D8D0BF6
     echo '# Kali linux repositories | Added by Katoolin' >> /etc/apt/sources.list
     echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
@@ -31,7 +30,7 @@ else
     echo "nameserver 1.1.1.1" > /etc/resolv.conf
     echo "nameserver 1.0.0.1" >> /etc/resolv.conf
 
-    sudo apt install -fy python python3 python3-pip golang nikto dmitry dnstracer dirb dirbuster gobuster exiftool binwalk radare2 gdb openvpn tor proxychains nginx
+    sudo apt install -fy python python3 python3-pip golang speedtest-cli nikto dmitry dnstracer dirb dirbuster gobuster exiftool binwalk radare2 gdb openvpn tor proxychains nginx
     #programming language support
     sudo python3 -m pip install --upgrade pip
 
