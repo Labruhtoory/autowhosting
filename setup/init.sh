dec4b8e578d1ca8d7e65b0f457a83791179940c2
Labruhtooryboi <67720330+Labruhtooryboi@users.noreply.github.com>
2021-04-05 10:11:59 -0900

    Update init.sh

--- setup/init.sh
+++ setup/init.sh
@@ -19,6 +19,7 @@ else
     apt-key adv --keyserver pool.sks-keyservers.net --recv-keys ED444FF07D8D0BF6
     echo '# Kali linux repositories | Added by Katoolin' >> /etc/apt/sources.list
     echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
+    sudo apt update
 fi
 
 if [ $tools == "n"]
