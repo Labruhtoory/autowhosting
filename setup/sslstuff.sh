48280f5fd9057b072e0af472d1889056737f63d3
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-04-05 15:13:05 -0900

    Update sslstuff.sh

--- setup/sslstuff.sh
+++ setup/sslstuff.sh
@@ -2,16 +2,13 @@
 mkdir -p /etc/ssl/private
 chmod 0750 /etc/ssl/private
 
-openssl req \ 
--newkey rsa:2048 -nodes -keyout /etc/ssl/private/serv.key \ 
--x509 -days 1825 -out /etc/ssl/certs/serv.crt \
--subj "/C=XX/ST=XX/L=XX/O=XX/OU=XX/CN=XX/emailAdress=X@X.lan"
+openssl req -newkey rsa:2048 -nodes -keyout /etc/ssl/private/serv.key -x509 -days 1825 -out /etc/ssl/certs/serv.crt -subj "/C=XX/ST=XX/L=XX/O=XX/OU=XX/CN=XX/emailAdress=X@X.lan"
 
 
 chmod 0400 /etc/ssl/certs/serv.crt
 chmod 0400 /etc/ssl/private/serv.crt
 
-
-mv /etc/nginx/conf.d/serv.conf /etc/nginx/conf.d/serv-ssl.conf
+touch /etc/nginx/conf.d/serv.conf 
+touch /etc/nginx/conf.d/serv-ssl.conf
 
 echo "see confs for creating config files"
