21c41aca9d121372e6b19a42e1d9915973849fa5
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-04-08 17:15:06 -0900

    Update ssl_serv_setup.sh

--- setup_serv/ssl_serv_setup.sh
+++ setup_serv/ssl_serv_setup.sh
@@ -8,16 +8,12 @@ cd /opt
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 echo "nameserver 1.0.0.1" >> /etc/resolv.conf
 
-sudo apt install -fy python python3 python3-pip golang speedtest-cli htop nginx mariadb-server php php7.2-cgi
+sudo apt install -fy python python3 python3-pip golang speedtest-cli htop nginx mariadb-server
 sudo python3 -m pip install --upgrade pip
 
-#basic vpn
-sudo wget --no-check-certificate https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-US2.zip
-unzip VPNBook.com-OpenVPN-US2.zip
-sudo rm -rf VPNBook.com-OpenVPN-US2.zip vpnbook-us2-tcp80.ovpn vpnbook-us2-tcp443.ovpn vpnbook-us2-udp53.ovpn 
-
-#db
-sudo 
+#db setup
+sudo mysql_secure_installation
+sudo apt install -fy php php7.2-cgi mongodb
 
 #certbot
 sudo apt-get install software-properties-common
@@ -28,6 +24,17 @@ sudo apt-get install python-certbot-nginx
 sudo certbot --nginx
 sudo systemctl restart nginx
 
+#basic vpn
+sudo wget --no-check-certificate https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-US2.zip
+unzip VPNBook.com-OpenVPN-US2.zip
+sudo rm -rf VPNBook.com-OpenVPN-US2.zip vpnbook-us2-tcp80.ovpn vpnbook-us2-tcp443.ovpn vpnbook-us2-udp53.ovpn 
+
+#dns leak test
+git clone https://github.com/macvk/dnsleaktest.git
+go build -o /usr/bin/dnsleaktest dnsleaktest/dnsleaktest.go
+chmod 755 /usr/bin/dnsleaktest
+rm -rf dnsleaktest/
+
 echo " don't forget to add a cron job 'sudo certbot renew'"
 echo "crontab -e"
 echo "0 0   1 * * sudo certbot renew"
@@ -36,5 +43,4 @@ echo ""
 echo ""
 echo ""
 echo "Also dont forget to make services for server apps!"
-echo "see service template"
 echo "Done!"
