b28fe98928a1b3c7eddd193313fec7d06510476b
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-04-10 11:32:50 -0900

    Update serv_setup.sh

--- setup_serv/serv_setup.sh
+++ setup_serv/serv_setup.sh
@@ -114,7 +114,7 @@ sudo mount /dev/$answ /dbs
 echo "/dev/$answ        /dbs        ext4    defaults      0      0" >> /etc/fstab
 echo "Ok, /dev/$answ with ext4 filsystem is prepped for mounting on boot"
 clear
-##############################    MariaDB (MySQL) Install and Data Migration   ##############################
+##############################    MariaDB (MySQL) for Wordpress Install, Data Migration, Setup, and Config   ##############################
 echo "Migrating MariaDB data to mounted disk....."
 sudo systemctl stop mariadb
 sudo rsync -rltDvz /var/lib/mysql /dbs
@@ -144,7 +144,7 @@ echo "EXIT"
 echo ""
 sudo mysql -u root -p
 clear
-##############################    MongoDB (NoSQL) for Wordpress Install, Data Migration, Setup, and Config   ##############################
+##############################    MongoDB (NoSQL) Install and Data Migration   ##############################
 echo "Migrating Mongo data to mounted disk....."
 sudo systemctl stop mongodb
 sudo rsync -rltDvz /var/lib/mongodb /dbs
