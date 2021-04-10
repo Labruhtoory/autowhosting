60f22b398bb882a968e6a06fcfef70095c671f19
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-04-09 22:12:30 -0900

    Update serv_setup.sh

--- setup_serv/serv_setup.sh
+++ setup_serv/serv_setup.sh
@@ -4,6 +4,7 @@
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 echo "nameserver 1.0.0.1" >> /etc/resolv.conf
 
+#init installs 
 sudo apt install -fy python python3 python3-pip golang speedtest-cli htop nginx mariadb-server
 sudo python3 -m pip install --upgrade pip
 echo "making some slight adjustments to nginx"
@@ -12,17 +13,12 @@ sudo rm -rf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 mv serv-confs-defaults/wpdef-serv.conf /etc/nginx/conf.d
 clear
 
-#############################################################################################
-#########   REVIEW THAT MYSQL HAS PERMISSIONS TO EXTERNAL DRIVE'S FILESYSTEM    #############
-
-#########   CURRENT THING TO CHECK, CHANGE THE TYPE OF FILESYSTEM OF THE DRIVE (EXT4)   #####
 
 #MariaDB (MySQL) setup
 echo "setting up initial database drive, database support, and support for wordpress....."
 mv wordpress.config /opt/
 cd /var/www/
-mkdir /mnt/usbdb1
-echo "Please insert a drive with only one empty partition....."
+echo "Please insert a drive with only one partition....."
 echo "Have you plugged in a device? If not, now is the time to do it....."
 echo "Press 'c' to continue....."
 while : ; do
@@ -33,57 +29,37 @@ break
 fi
 done
 lsblk | grep "disk\|part"
-echo -n "What is the new partition to mount? (EX: sda1, sda2, sdb1 etc.....): "
-read answ
-
-
+read -p "What is the new partition to mount? (EX: sda1, sda2, sdb1 etc.....): " answ
 mkfs.ext4 /dev/$answ
-
-
 echo "mounting $answ"
 sudo mkdir /dbs
-sudo chown -R mysql:mysql /dbs/
 sudo mount /dev/$answ /dbs
-df -Th | grep $answ
-read -p "What kind of file system is the mounted drive? (EX: ext4, exfat, fat32, vfat etc.....): " answ2
-echo "In a separate terminal, edit /etc/fstab and add the following line....."
-echo "/dev/$answ        /dbs        $answ2    defaults      0      0"
+echo "/dev/$answ        /dbs        ext4    defaults      0      0" >> /etc/fstab
 "Press 'c' to continue....."
-while : ; do
-read -n 1 k <&1
-if [[ $k = c ]] ; then
-printf "Ok then, moving on....."
-break
-fi
-done
-echo "Ok, /dev/$answ with filsystem $answ2 is prepped for mounting on boot"
+echo "Ok, /dev/$answ with ext4 filsystem is prepped for mounting on boot"
 clear
 
-echo "Migrating MariaDB data to mounted disk. make sure you know your root passwd, if you dont, then run: sudo passwd root"
-sudo mysql_secure_installation
+echo "Migrating MariaDB data to mounted disk....."
 sudo systemctl stop mariadb
 sudo rsync -rltDvz /var/lib/mysql /dbs
-#cp -R /var/lib/mysql
-
-chmod -R 777 /dbs
-chmod -R 777 /var/lib/mysql
-sudo chown -R mysql:mysql /dbs
-sudo chown -R mysql:mysql /var/lib/mysql
-mysql -u root -p
-# \! cp -R /var/lib/mysql /dbs
-
-echo "In a sparate terminal, edit: /etc/mysql/mariadb.conf.d/50-server.cnf..."
-echo "Change datadir to /dbs "
+sudo chown -R mysql:mysql /dbs/
+sudo grep -R --color datadir /etc/mysql/*
+cp /etc/mysql/mariadb.conf.d/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf.bak
+sed -i "s+/var/lib/mysql+/dbs/mysql+gi" /etc/mysql/mariadb.conf.d/50-server.cnf
 sudo systemctl start mariadb
-#ln -s /mnt/usbdb1/mysql /var/www/
-#echo "created sybolic link folder for database drive $answ "
+clear
+
+echo "Make sure you know your root user passwd, if you dont, then run 'sudo passwd root' in a separate terminal"
+sudo mysql_secure_installation
+ln -s /dbs/ /var/www/
+echo "created sybolic link folder for database drive $answ in /var/www"
 echo "Mounted $answ to /dbs and migrated MariaDB data"
 clear
 
-#############################################################################################
+
 echo "setting up initial mysql wordpress database"
 echo "Now enter your MYSQL root passwd in the prompt below."
-echo "Run the folowing commands in the mysql> prompt: "
+echo "Run the folowing commands in the mysql> prompt.....and leave quotes and ticks in commands!"
 echo "CREATE DATABASE wordpress;"
 echo "CREATE USER newuserhere IDENTIFIED BY 'newpasswordhere';"
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO newuserhere;"
