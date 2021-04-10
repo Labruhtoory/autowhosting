1d6ef0cdaf21654836b7f435b064dfc2ba36b00a
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-04-09 23:26:50 -0900

    Update serv_setup.sh

--- setup_serv/serv_setup.sh
+++ setup_serv/serv_setup.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
-
-#networking
+#initial comments
+echo "*Quick note, say yes to and fill out all services' prompts. Just makes the process easier :)"
+echo ""
+echo "Before you begin, make sure that you keep track of your credentials that you setup....."
+echo "Keep in mind, you will need to supply your own credentials and information for the following services....."
+echo ""
+echo "MariaDB (MySQL):"
+echo "  > Your system/server root account password for creating and configuring the root database account"
+echo "  > A username to create a new user for accessing the new wordpress database"
+echo "  > A password for this new user"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Press 'c' to continue....."
+while : ; do
+read -n 1 k <&1
+if [[ $k = c ]] ; then
+printf "Ok then, moving on....."
+break
+fi
+done
+clear
+##############################   Networking   ##############################
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 echo "nameserver 1.0.0.1" >> /etc/resolv.conf
-
-#init installs 
+##############################   Init Installs, and Copying Templates for Configs    ##############################
 sudo apt install -fy python python3 python3-pip golang speedtest-cli htop nginx mariadb-server
 sudo python3 -m pip install --upgrade pip
+clear
 echo "making some slight adjustments to nginx"
 systemctl enable nginx && systemctl restart nginx
 sudo rm -rf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 mv serv-confs-defaults/wpdef-serv.conf /etc/nginx/conf.d
+go get github.com/gorilla/websocket
 clear
-
-
-#MariaDB (MySQL) setup
+##############################    MariaDB (MySQL) for Wordpress Install, Setup, Data Migration, and Config   ##############################
 echo "setting up initial database drive, database support, and support for wordpress....."
 mv wordpress.config /opt/
 cd /var/www/
@@ -28,6 +53,7 @@ printf "Ok then, moving on....."
 break
 fi
 done
+clear
 lsblk | grep "disk\|part"
 read -p "What is the new partition to mount? (EX: sda1, sda2, sdb1 etc.....): " answ
 mkfs.ext4 /dev/$answ
@@ -35,10 +61,8 @@ echo "mounting $answ"
 sudo mkdir /dbs
 sudo mount /dev/$answ /dbs
 echo "/dev/$answ        /dbs        ext4    defaults      0      0" >> /etc/fstab
-"Press 'c' to continue....."
 echo "Ok, /dev/$answ with ext4 filsystem is prepped for mounting on boot"
 clear
-
 echo "Migrating MariaDB data to mounted disk....."
 sudo systemctl stop mariadb
 sudo rsync -rltDvz /var/lib/mysql /dbs
@@ -48,18 +72,15 @@ cp /etc/mysql/mariadb.conf.d/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.c
 sed -i "s+/var/lib/mysql+/dbs/mysql+gi" /etc/mysql/mariadb.conf.d/50-server.cnf
 sudo systemctl start mariadb
 clear
-
 echo "Make sure you know your root user passwd, if you dont, then run 'sudo passwd root' in a separate terminal"
 sudo mysql_secure_installation
 ln -s /dbs/ /var/www/
 echo "created sybolic link folder for database drive $answ in /var/www"
 echo "Mounted $answ to /dbs and migrated MariaDB data"
 clear
-
-
 echo "setting up initial mysql wordpress database"
-echo "Now enter your MYSQL root passwd in the prompt below."
-echo "Run the folowing commands in the mysql> prompt.....and leave quotes and ticks in commands!"
+echo "Enter your system root account passwd in the prompt below."
+echo "After that, run the folowing quieries in the mysql> prompt.....and leave quotes and ticks in commands!"
 echo "CREATE DATABASE wordpress;"
 echo "CREATE USER newuserhere IDENTIFIED BY 'newpasswordhere';"
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO newuserhere;"
@@ -67,11 +88,7 @@ echo "FLUSH PRIVILEGES;"
 echo "EXIT"
 sudo mysql -u root -p
 clear
-clear
-
-
-
-#Wordpress init setup
+##############################    Wordpress Site Init Install, Setup, and Config    ##############################
 echo "Setting up wordpress"
 cd /var/www/
 sudo wget http://wordpress.org/latest.tar.gz
@@ -80,32 +97,67 @@ rm -rf latest.tar.gz
 mv /opt/wordpress.config /var/www/wordpress/
 cd /var/www/wordpress
 sudo cp wp-config-sample.php wp-config.php
+clear
+echo "Editing wordpress mysql config...."
+
+
+
+
+
+
+
+
+
+sed -i "s/database_name_here/wordpress/gi" /var/www/wordpress/wp-config.php
+read -p "What is your new MariaDB (MySQL) username?" unanmer
+sed -i "s/username_here/$unamer/gi" /var/www/wordpress/wp-config.php
+read -p "What is your new MariaDB (MySQL) password?" passwder
+sed -i "s/password_here/$passwder/gi" /var/www/wordpress/wp-config.php
+clear
+echo "In a separate terminal, make the following changes to /var/www/wordpress/wp-config.php ....."
+echo ""
+echo ""
+echo "Find the KEY lines, EX: define('AUTH_KEY', / define('SECURE_AUTH_KEY'  and enter the values of the genterated salts below"
+echo "copy what is between the single quotes below into its respective place in /var/www/wordpress/wp-config.php"
+echo "(make sure to put it betweensingle quotes)"
+echo ""
+echo ""
+echo ""
+echo ""
 curl -s https://api.wordpress.org/secret-key/1.1/salt/
-echo "In a separate terminal, make the following changes to wp-config.php....."
-echo "find the MYSQL Settings lines and enter 'wordpress' for DB_NAME, 'labruhtooryboi' for DB_USER, and 'toortoor' for DB_PASSWORD"
-echo "now find the KEY lines, EX: define('AUTH_KEY', / define('SECURE_AUTH_KEY'  and enter the values of the previously genterated salts"
-echo "if you have any questions, pleas look at: https://www.nginx.com/blog/installing-wordpress-with-nginx-unit/"
+echo ""
+echo ""
+echo ""
+echo ""
 echo "When your done editing wp-config.php, STOP, WAIT, press c to continue the script."
 while : ; do
 read -n 1 k <&1
 if [[ $k = c ]] ; then
+echo ""
 printf "Ok then, moving on....."
 break
 fi
 done
-
 sudo chown -R wpuser:www-data /var/www/wordpress
 sudo find /var/www/wordpress -type d -exec chmod g+s {} \;
 sudo chmod g+w /var/www/wordpress/wp-content
 sudo chmod -R g+w /var/www/wordpress/wp-content/themes
 sudo chmod -R g+w /var/www/wordpress/wp-content/plugins
-
+##############################    NGINX Unit Source, Config, Build, and Install     ##############################
 cd /opt/
 sudo apt install -fy php php7.2-cgi php7.0 mongodb
 sudo apt remove -y apache2 apache2-utils
 clear
 
+                                           
+                                           
+                                           
+                                           ###   need to figure out installing nginx unit
+
 echo "Installing nginx unit for wordpress....."
+
+
+
 cd /opt/
 # nginx unit source download, compilation, and install
 sudo apt install -fy build-essential golang
@@ -118,37 +170,49 @@ cd unit/
 ./configure --state=/var/lib/unit --log=/var/log/unit.log --control=unix:/run/control.unit.sock --prefix=/usr/local/ --openssl
 ./configure go && ./configure java && ./configure nodejs && ./configure perl && ./configure php && ./configure python && ./configure ruby
 make && make install 
-
-#ssl certbot
 clear
-echo "installing certbot....."
+##############################    SSL Cert and Key Gen    ##############################
+#ssl certbot
+echo "Installing CertBot....."
+echo ""
 read -p "What domain name would you like to use for your website?> " $domain
+
+                                        ###   this left a blank space
+
 sed -i "s/domain.dns/$domain/gi" /etc/nginx/conf.d/wpdef-serv.conf
+
+
+                                        ### install certbot in the beginning of the script
+                                        ### and, edit nginx .conf template bc certbot threw an error
+                            ### Error while running nginx -c /etc/nginx/nginx.conf -t.
+                            ### nginx: [emerg] open() "/etc/letsencrypt/options-ssl-nginx.conf" failed (2: No such file or directory) in /etc/nginx/conf.d/wpdef-serv.conf:25
+                            ### nginx: configuration file /etc/nginx/nginx.conf test failed
+                            ###
+                            
+                            
 sudo apt-get install -fy software-properties-common
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
 sudo apt-get install -fy python-certbot-nginx
-
-sudo certbot --nginx
+sudo apt remove -fy apache2-data
+mv /var/wwww/html/index.nginx-debian.html /var/www/html/index.html
+mv /var/www/html/index.html /var/www/index.html
+rm -rf /var/www/html/
 sudo systemctl restart nginx
-
-#basic vpn
-echo "getting a vpn config file from"
+clear
+##############################    Xtra Free Vpn (Optional)    ##############################
+echo "Getting a free vpn config file from vpnbook.com/freevpm....."
 sudo wget --no-check-certificate https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-US2.zip
 unzip VPNBook.com-OpenVPN-US2.zip
 sudo rm -rf VPNBook.com-OpenVPN-US2.zip vpnbook-us2-tcp80.ovpn vpnbook-us2-tcp443.ovpn vpnbook-us2-udp53.ovpn 
-
+clear
 #dns leak test
-echo "installing dns leak test"
+echo "installing a dns leak test, run by commad 'dnsleaktest'"
 git clone https://github.com/macvk/dnsleaktest.git
 go build -o /usr/bin/dnsleaktest dnsleaktest/dnsleaktest.go
 chmod 755 /usr/bin/dnsleaktest
 rm -rf dnsleaktest/
-
-#xtra golang packages 
-go get github.com/gorilla/websocket
-go get unit.nginx.org/go
-
+##############################    Closing Comments   ##############################
 echo " don't forget to add a cron job 'sudo certbot renew'"
 echo "crontab -e"
 echo "0 0   1 * * sudo certbot renew"
@@ -161,12 +225,15 @@ echo ""
 echo ""
 echo ""
 echo ""
-echo "Check vpn creds at: https://www.vpnbook.com/freevpn"
+echo "Check credentials for the vpn at: https://www.vpnbook.com/freevpn"
 echo "Check under: Free OpenVPN"
+echo "Run the vpn with 'sudo openvpn vpnconfigfilehere' ....."
+echo ""
+echo ""
+echo ""
+echo "For more information on how to setup wordpress on nginx, visit: https://www.nginx.com/blog/installing-wordpress-with-nginx-unit/"
 echo ""
 echo ""
 echo ""
 echo ""
-echo "For mor information visit: https://www.nginx.com/blog/installing-wordpress-with-nginx-unit/"
-
 echo "Done!"
