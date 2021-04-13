90bb825f60b43b5158bcbfd15649215e536afdf6
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-04-13 14:49:09 -0900

    Update init_setup.sh

--- setup_serv/init_setup.sh
+++ setup_serv/init_setup.sh
@@ -134,5 +134,10 @@ printf "Ok then, moving on....."
 break
 fi
 done
+##############################    New User For Web Management   ##############################
+echo "Seting up new user for website management....."
+echo "In a seperate terminal, run the following 'adduser wordy'"
+echo "All information is optional except for a passwd, remeber it....."
+mkdir -p /home/wordy/logs
 chmod +x new_site.sh
 ./new_site.sh
