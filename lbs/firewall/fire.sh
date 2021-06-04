708aea1b9557b6981a242cc1e6a84612c9c40a5f
Labruhtoory <67720330+Labruhtoory@users.noreply.github.com>
2021-06-03 21:40:06 -0700

    Update fire.sh

--- lbs/firewall/fire.sh
+++ lbs/firewall/fire.sh
@@ -5,20 +5,17 @@ sudo ufw reset
 
 # server maintenence
 
-clone
 echo "Allowing Server Maintenence..."
 sudo ufw allow ssh
 
 # make ports available to be used for server apps
 
-clone
 echo "Making 11 ports avalible for misc server apps..."
 sudo ufw allow 8000:8010/tcp
 sudo ufw allow 8000:8010/udp
 
 # manage web requests
 
-clone
 echo "Allowing standard http+..."
 sudo ufw allow in on eth0 to any port 80
 sudo ufw allow in on eth0 to any port 443
@@ -30,7 +27,6 @@ sudo ufw allow out on eth0 to any port 8080
 
 # full send
 
-clone
 echo "Starting Firewall..."
 sudo ufw enable
 echo "Done!!!"
